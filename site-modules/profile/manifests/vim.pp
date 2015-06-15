cd73858122630f86308ff108149aa5072506c394
Renaud Farga <renaud@farganel.com>
2015-06-15 16:46:13 +0100

    bad name

--- site-modules/profile/manifests/vim.pp
+++ site-modules/profile/manifests/vim.pp
@@ -4,8 +4,10 @@ class profile::vim {
 
 
 class { "::vim":
-user => "user",
+user => "osboxes",
+home_dir => "/home/osboxes",
 }
 
-vim::plugin { "https://github.com/scrooloose/nerdtree.git":}
+vim::plugin { 'nerdtree':
+source => "https://github.com/scrooloose/nerdtree.git"}
 }
