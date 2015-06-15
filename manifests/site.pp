1f0268b9366ff8af668f7d67ad04df708fe72110
Eric de Beaumont <eric.debeaumont@amadeus.com>
2015-06-15 16:09:44 +0100

    Add vim profile and devserver role to eric01

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@ node "puppetmaster.c2c" {
   include role::puppetmaster
 }
 
+node "eric01.dev.c2c" {
+  include role::devserver
+}
+
 node /dev\.c2c/ {
   package { 'git':
     ensure => installed,
---
+++ site-modules/role/manifests/devserver.pp
@@ -0,0 +1,3 @@
+class role::devserver {
+  include profile::vim
+}
