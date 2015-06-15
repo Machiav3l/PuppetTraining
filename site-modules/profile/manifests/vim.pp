8dfb4e9a0cd80bd47e723958b308c41ffe03d501
Maxime SIQUIER <msiquier.trololo@c2c.com>
2015-06-15 16:48:48 +0100

    or not

--- site-modules/profile/manifests/vim.pp
+++ site-modules/profile/manifests/vim.pp
@@ -1,10 +1,10 @@
 class profile::vim {
-  class { "::vim":
-    user => "osboxes.org",
-    home_dir => "home/user",
+  class { '::vim':
+    user => 'osboxes',
+    home_dir => 'home/user',
   }
 
-  vim::plugin { 'nerdtree' :
+  vim::plugin { 'nerdtree':
     source => 'https://github.com/scrooloose/nerdtree.git',
   }
 }
