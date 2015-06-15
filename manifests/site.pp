02628d112cd33b1a3fd4bd2ef067fdddacb442d9
Renaud Farga <renaud@farganel.com>
2015-06-15 16:14:37 +0100

    Add desktop qnd VIM

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,8 +2,14 @@ node "puppetmaster.c2c" {
   include role::puppetmaster
 }
 
+node "jahwisdom.dev.c2c" {
+include role::desktop
+}
+
 node /dev\.c2c/ {
   package { 'git':
     ensure => installed,
   }
 }
+
+
