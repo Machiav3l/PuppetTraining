b1bb411f3e205081edd2f3a4cdaa29260d361171
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-06-16 09:31:38 +0200

    Do not use vim::rc for now

--- site-modules/profile/manifests/vim.pp
+++ site-modules/profile/manifests/vim.pp
@@ -46,15 +46,15 @@ class profile::vim (
     provider => 'gem',
   }
 
-  vim::rc { 'syntastic':
-    content => '
-      " syntastic
-      set statusline+=%#warningmsg#
-      set statusline+=%{SyntasticStatuslineFlag()}
-      set statusline+=%*
-      let g:syntastic_always_populate_loc_list = 1
-      let g:syntastic_auto_loc_list = 1
-      let g:syntastic_check_on_wq = 0
-    ',
-  }
+  #  vim::rc { 'syntastic':
+  #    content => '
+  #      " syntastic
+  #      set statusline+=%#warningmsg#
+  #      set statusline+=%{SyntasticStatuslineFlag()}
+  #      set statusline+=%*
+  #      let g:syntastic_always_populate_loc_list = 1
+  #      let g:syntastic_auto_loc_list = 1
+  #      let g:syntastic_check_on_wq = 0
+  #    ',
+  #  }
 }
