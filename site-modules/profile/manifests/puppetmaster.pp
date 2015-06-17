dd5e397d4a948cffbc6b64b4d1231ff1f822ffb3
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-06-17 14:09:32 +0200

    Add resolver to puppetmaster

--- site-modules/profile/manifests/puppetmaster.pp
+++ site-modules/profile/manifests/puppetmaster.pp
@@ -2,4 +2,9 @@ class profile::puppetmaster {
   package { 'puppetmaster':
     ensure => installed,
   }
+
+  augeas { 'use google resolver':
+    context => '/files/etc/resolv.conf',
+    changes => 'set nameserver[.="8.8.8.8"] 8.8.8.8',
+  }
 }
