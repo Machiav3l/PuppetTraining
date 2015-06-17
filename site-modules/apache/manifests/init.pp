64084cfd7aa505ec5ba78f043617b1f60659826a
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-06-17 10:56:13 +0200

    Use variables and conffile source

--- site-modules/apache/manifests/init.pp
+++ site-modules/apache/manifests/init.pp
@@ -1,17 +1,40 @@
 class apache {
+  case $::osfamily {
+    'debian': {
+      $package = 'apache2'
+      $conffile = '/etc/apache2/apache2.conf'
+      $service = 'apache'
+      $user = 'www-data'
+      $group = 'www-data'
+    }
+
+    'redhat': {
+      $package = 'httpd'
+      $conffile = '/etc/httpd/conf/httpd.conf'
+      $service = 'httpd'
+      $user = 'apache'
+      $group = 'apache'
+    }
+
+    default: {
+      fail "Unsupported OS family ${::osfamily}"
+    }
+  }
+
   package { 'apache':
     ensure => installed,
-    name   => 'httpd',
+    name   => $package,
   } ->
   file { 'apache conf':
     ensure => file,
-    path   => '/etc/httpd/httpd.conf',
-    owner  => 'apache',
-    group  => 'apache',
+    path   => $conffile,
+    owner  => $user,
+    group  => $group,
     mode   => '0644',
+    source => "puppet:///modules/apache/apache.${::osfamily}.conf",
   } ~>
   service { 'apache':
     ensure => running,
-    name   => 'httpd',
+    name   => $service,
   }
 }
