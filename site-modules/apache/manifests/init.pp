ecac09ca60ddcd7b034371182f516dc3e1b3f39c
Maxime SIQUIER <msiquier.trololo@c2c.com>
2015-06-17 10:03:13 +0100

    Fix error again and again

--- site-modules/apache/manifests/init.pp
+++ site-modules/apache/manifests/init.pp
@@ -25,7 +25,7 @@ class apache {
     owner  => $user,
     group  => $user,
     mode   => '0644',
-    source => $source = "puppet:///site-modules/apache/apache.${::osfamily}.conf",
+    source => "puppet:///site-modules/apache/apache.${::osfamily}.conf",
   } ~>
   service { 'apache':
     ensure => running,
