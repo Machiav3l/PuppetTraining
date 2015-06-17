3b800c64161a425210719696cfadb727ddcca0dc
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-06-17 14:16:21 +0200

    Add netrc rndwwww

--- site-modules/profile/manifests/puppetmaster.pp
+++ site-modules/profile/manifests/puppetmaster.pp
@@ -7,4 +7,13 @@ class profile::puppetmaster {
     context => '/files/etc/resolv.conf',
     changes => 'set nameserver[.="8.8.8.8"] 8.8.8.8',
   }
+
+  augeas { 'netrc rndwwww':
+    incl    => '/root/.netrc',
+    lens    => 'Netrc.lns',
+    changes => [
+      'set rndwww/login admin',
+      'set rndwww/password p@SSw0rd',
+    ]
+  }
 }
